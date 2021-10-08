bdf81889341ed044181a31a54fa05c787b89aaf4
Renata Dal Bello Sanchez <reenadbsanchez@gmail.com>
2021-10-08 14:57:58 -0300

    mostar mais do texto e mostrar formulario funcionando

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ function mostrarIdiomas() {
         listaIdiomas.style.display = 'none'
     }
 }
+
+
+function mostrarTexto(){
+    const maisTexto = document.getElementById("textoEscondido");
+    const botao = document.getElementById("botaoVerMais");
+    if (maisTexto.style.display == "none"){
+        maisTexto.style.display="inline";
+        botao.innerHTML="ver menos";
+
+    } else{
+        maisTexto.style.display="none";
+    }
+
+    if (maisTexto.style.display == "none"){
+        botao.innerHTML="ver mais";
+    }
+}
+
+function criaTopico(){
+    const discussao = document.getElementById("discussao");
+    const formulario = document.getElementById("divFormulario");
+    if (discussao.style.display == "flex");{
+    discussao.style.display ="none";
+    formulario.style.display ="flex";
+    }
+}
