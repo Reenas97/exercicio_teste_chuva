e51b6145a50289b7b5eaa39e6304e8b8329c826b
Renata Dal Bello Sanchez <reenadbsanchez@gmail.com>
2021-10-11 14:43:21 -0300

    codigo minimamente responsivo

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function mostrarIdiomas() {
 function mostrarTexto(){
     const maisTexto = document.getElementById("textoEscondido");
     const botao = document.getElementById("botaoVerMais");
-    if (maisTexto.style.display == "none"){
+    if (maisTexto.style.display != "inline"){
         maisTexto.style.display="inline";
         botao.innerHTML="ver menos";
 
@@ -34,28 +34,64 @@ function criaTopico(){
     }
 }
 
-function enviaFeedback(){
+// function enviaFeedback(){
+//     const feedback = document.getElementById("feedbadivFormulariockEnviado");
+//     const discussao = document.getElementById("discussao");
+//     const formulario = document.getElementById("divFormulario");
+//     if (feedback.style.display == "none");{
+//         feedback.style.display = "flex";
+//         discussao.style.display = "none";
+//         formulario.style.display = "none";
+//     }
+// }
+
+function topicoExpandido(){
+   const respostas = document.getElementById("cardTopico");
+   if (respostas.style.display !="flex"){
+    respostas.style.display ="flex";
+   } else {
+       respostas.style.display = "none";
+   }
+
+
+
+}
+
+function topicoExpandido2(){
+    const respostas2 = document.getElementById("cardTopico2");
+    if (respostas2.style.display !="flex") {
+        respostas2.style.display ="flex";
+    } else {
+        respostas2.style.display ="none";
+    }
+
+}
+
+function mostraMenu(){
+    const menu = document.getElementById("menu");
+    if (menu.style.display !="flex") {
+        menu.style.display ="flex";
+    } else {
+        menu.style.display ="none";
+    }
+
+}
+
+function enviaFeedback() {
+    const assunto = document.getElementById ('assunto').value;
+    const conteudo = document.getElementById ('conteudo').value;
+
     const feedback = document.getElementById("feedbackEnviado");
     const discussao = document.getElementById("discussao");
     const formulario = document.getElementById("divFormulario");
+    document.getElementById("assuntoNovo").textContent= assunto;
+    document.getElementById("perguntaNova").textContent= conteudo;
     if (feedback.style.display == "none");{
         feedback.style.display = "flex";
         discussao.style.display = "none";
         formulario.style.display = "none";
     }
-}
 
-function topicoExpandido(){
-   const respostas2 = document.getElementById("cardTopico2");
-   const respostas = document.getElementById("cardTopico");
-   if (respostas.style.display =="none");{
-    respostas.style.display ="flex";
-   } 
-
-   if (respostas2.style.display =="none");{
-    respostas2.style.display ="flex";
-   } 
 
 
 }
-
